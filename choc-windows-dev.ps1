789b01fd3e099e469462f12b7d5518d3f87baeba
Jess White <Jess.White@retig.local>
2017-11-15 10:20:29 +0000

    Chocolatey install list

---
+++ choc-windows-dev.ps1
@@ -0,0 +1,15 @@
+# Install Applications
+choco install fiddler4
+choco install sublimetext3
+choco install notepadplusplus
+choco install visualstudiocode
+choco install resharper-platform
+choco install sql-server-management-studio
+choco install sql-server-express
+choco install nodejs --version 6.11.4
+
+# Personal
+choco install spotify
+
+# Install Utilities
+choco install 7zip
